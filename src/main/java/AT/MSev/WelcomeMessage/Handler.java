4d4e58ee968d25897e1892f49bb5af1b22961a7d
FHanko <FlorianHanko@hotmail.com>
2018-07-14 03:12:06 +0200

    Added custom public message

--- src/main/java/AT/MSev/WelcomeMessage/Handler.java
+++ src/main/java/AT/MSev/WelcomeMessage/Handler.java
@@ -1,9 +1,14 @@
 package AT.MSev.WelcomeMessage;
 
+import net.md_5.bungee.api.chat.TextComponent;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.plugin.Plugin;
+
+import java.awt.*;
 
 public class Handler implements Listener {
 
@@ -16,8 +21,16 @@ public class Handler implements Listener {
     @EventHandler
     void OnJoin(PlayerJoinEvent e)
     {
+        e.setJoinMessage("");
+        for (Player p:
+            plugin.getServer().getOnlinePlayers()) {
+            p.sendMessage(String.format(Util.replaceColors(plugin.getConfig().getString("publicmessage")), e.getPlayer().getDisplayName()));
+        }
+
         e.getPlayer().sendMessage(
                Util.replaceColors(plugin.getConfig().getString("message"))
         );
     }
+
+
 }
